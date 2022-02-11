a6f204d6ab29ade32a3a6330614cefa165601aff
HP <bhavani12jn@gmail.com>
2022-02-11 22:31:18 +0530

    sample

--- products/src/main/java/com/products/demo/repo/ProductRepo.java
+++ products/src/main/java/com/products/demo/repo/ProductRepo.java
@@ -1,9 +1,22 @@
 package com.products.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import com.products.demo.model.ProductModel;
 
 public interface ProductRepo extends JpaRepository<ProductModel, Integer> {
+	@Query(value="SELECT * FROM product WHERE Name LIKE %:name%",nativeQuery = true)
+		public List<ProductModel>getByName(@Param("name")String name);
+		
+		
+		
+	}
+	
 
-}
+
+
